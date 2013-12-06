61726b492e0a7983db1b12c68be195d282f0ef9c
HangHoang <hangt11210@gmail.com>
2013-12-06 14:40:07 +0700

    four commit

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,6 +2,6 @@ class Product < ActiveRecord::Base
 	belongs_to :product_type, :order => "created_at DESC"
 	attr_accessible :product_type_id, :product_name, :product_description, :product_cost
 	validates :product_name, uniqueness: {message: "Name of product is exists" }, presence: true
-	validates :product_cost, presence: true, format: { with: /[0-9]/,
+	validates :product_cost, presence: true, format: { with: /\d+\.+[0-9]/,
     message: "only allows number" }
 end
